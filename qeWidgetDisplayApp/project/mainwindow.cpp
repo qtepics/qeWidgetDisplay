378f7cb2ea2ffe41a771b2473c63148b4a1939df
Andrew Starritt <andrew.starritt@gmail.com>
2021-11-27 15:12:22 +1100

    Replace deprecated call to setDisplayAlarmState for example display application.

--- qeWidgetDisplayApp/project/mainwindow.cpp
+++ qeWidgetDisplayApp/project/mainwindow.cpp
@@ -1,6 +1,9 @@
 /*  MainWindow.cpp
  *
- *  This file is part of the EPICS QT Framework, initially developed at the Australian Synchrotron.
+ *  This file is part of the EPICS QT Framework, initially developed at the
+ *  Australian Synchrotron.
+ *
+ *  Copyright (c) 2014-2021 Australian Synchrotron
  *
  *  The EPICS QT Framework is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
@@ -14,8 +17,6 @@
  *
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with the EPICS QT Framework.  If not, see <http://www.gnu.org/licenses/>.
- *
- *  Copyright (c) 2014 Australian Synchrotron
  *
  *  Author:
  *    Andrew Rhyder
@@ -226,8 +227,7 @@ void MainWindow::createQEComboBox()
 
     // Don't display the alarm state. This is done by setting the background colour and
     // we will be setting the background ourselves and don't want it overwritten
-    // USE OF setDisplayAlarmState() is deprecated. USE setDisplayAlarmStateOption() instead
-    qecb->setDisplayAlarmState( false );
+    qecb->setDisplayAlarmStateOption ( standardProperties::DISPLAY_ALARM_STATE_NEVER );
 
     // Set the variable name using the access function for the variable name property.
     // This will activate the widget using the variable name and the current macro substitutions.
